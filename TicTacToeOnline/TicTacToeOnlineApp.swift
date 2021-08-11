8691241258823fd4038fded1a34e048e7c8bd703
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-08-11 09:26:23 +0300

    Initial Commit

---
+++ TicTacToeOnline/TicTacToeOnlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeOnlineApp.swift
+//  TicTacToeOnline
+//
+//  Created by ramil on 11.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeOnlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
